efc02ffff99e3b2ef1c9909d9312a2b2251e87f4
David Wesst <david@cocobokostudios.com>
2024-10-30 19:01:08 +0000

    refactor. Migrated slack page and assets into 11ty.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,10 @@
 export default async function(eleventyConfig) {
     eleventyConfig.setInputDirectory("src");
+
+    // passthroughs are relative to root not input directory
+    eleventyConfig.addPassthroughCopy("src/images");
+    eleventyConfig.addPassthroughCopy("src/styles");
+    eleventyConfig.addPassthroughCopy("src/scripts");
 };
 
 export const config = {};
