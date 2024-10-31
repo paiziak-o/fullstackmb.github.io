4c8d3b05db84836d6468c08c848a5538269d9a8e
David Wesst <david@cocobokostudios.com>
2024-10-31 17:25:08 +0000

    refactor. Passing through CNAME file for site hosting.

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,6 +5,7 @@ export default async function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/images");
     eleventyConfig.addPassthroughCopy("src/styles");
     eleventyConfig.addPassthroughCopy("src/scripts");
+    eleventyConfig.addPassthroughCopy("src/CNAME");
 };
 
 export const config = {};
