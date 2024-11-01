d990efcb2daec7201114c6e3c917a4583a3e122e
David Wesst <david@cocobokostudios.com>
2024-11-01 15:36:46 -0500

    refactor. Ported layout and most styles to next event section.

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,6 +6,15 @@ export default async function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/styles");
     eleventyConfig.addPassthroughCopy("src/scripts");
     eleventyConfig.addPassthroughCopy("src/CNAME");
+
+    // dev server configuraiton
+    eleventyConfig.setServerOptions({
+        watch: [
+            "src/**/*.liquid",
+            "src/**/*.css",
+            "src/**/*.js"
+        ]
+    });
 };
 
 export const config = {};
